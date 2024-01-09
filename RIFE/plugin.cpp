85a5836c46e4065dc85af615f6798cdfa0f0dcf8
styler00dollar <styler00dollar64@gmail.com>
2024-01-09 03:37:05 +0100

    editing model range

--- RIFE/plugin.cpp
+++ RIFE/plugin.cpp
@@ -203,8 +203,8 @@ static void VS_CC rifeCreate(const VSMap* in, VSMap* out, [[maybe_unused]] void*
         if (err)
             d->skipThreshold = 60.0;
 
-        if (model < 0 || model > 40)
-            throw "model must be between 0 and 40 (inclusive)";
+        if (model < 0 || model > 45)
+            throw "model must be between 0 and 45 (inclusive)";
 
         if (factorNum < 1)
             throw "factor_num must be at least 1";
