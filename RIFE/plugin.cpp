db73ccd7f0a83e8da7a88643a1a7ffea3492e6ad
TNTwise <tntwise3@gmail.com>
2024-09-19 10:48:01 -0500

    rife-v4.25

--- RIFE/plugin.cpp
+++ RIFE/plugin.cpp
@@ -203,8 +203,8 @@ static void VS_CC rifeCreate(const VSMap* in, VSMap* out, [[maybe_unused]] void*
         if (err)
             d->skipThreshold = 60.0;
 
-        if (model < 0 || model > 70)
-            throw "model must be between 0 and 70 (inclusive)";
+        if (model < 0 || model > 71)
+            throw "model must be between 0 and 71 (inclusive)";
 
         if (factorNum < 1)
             throw "factor_num must be at least 1";
@@ -490,12 +490,15 @@ static void VS_CC rifeCreate(const VSMap* in, VSMap* out, [[maybe_unused]] void*
                 modelPath += "/rife-v4.24_ensembleTrue";
                 break;
             case 68:
-                modelPath += "/sudo_rife4_ensembleFalse_fastTrue";
+                modelPath += "/rife-v4.25_ensembleFalse";
                 break;
             case 69:
-                modelPath += "/sudo_rife4_ensembleTrue_fastFalse";
+                modelPath += "/sudo_rife4_ensembleFalse_fastTrue";
                 break;
             case 70:
+                modelPath += "/sudo_rife4_ensembleTrue_fastFalse";
+                break;
+            case 71:
                 modelPath += "/sudo_rife4_ensembleTrue_fastTrue";
                 break;
             
